e3c16488d2b513926beeec61f232acbf0b8f5c85
Rajat Pugaliya <Rajat.Pugaliya@softline.com>
2019-01-20 21:33:21 +0530

    Add project files.

---
+++ DemoEmerson/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoEmerson.Startup))]
+namespace DemoEmerson
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
